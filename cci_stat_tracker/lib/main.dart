f68a76fc0f186440b962db4f1609fd4e1b0f4257
Ryan Crowley <rcrowl2@uic.edu>
2023-11-14 17:09:01 -0600

    stats page is functioning, considering updating to ListTile layout

--- cci_stat_tracker/lib/main.dart
+++ cci_stat_tracker/lib/main.dart
@@ -26,13 +26,27 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  // var current = WordPair.random();  --this was from the codelab
+
+   var stats = [0, 0, 0, 0, 0, 0, 0, 0]; // list of stats (init solo, init partner, sc solo, sc partner, etc...)
+
   
-  // USE THIS AS THE METHOD TO CALL DATABAST, ETC
+  // USE THIS AS THE METHOD TO CALL DATABASE, ETC
   void login() {
     print('user logged in');
     notifyListeners();
   }
+
+  void incStat(var theStat) { // increment specific stat in stats list
+    stats[theStat]++;
+    notifyListeners();
+  }
+  void decStat(var theStat) { // decrement specific stat in stats list 
+    if(stats[theStat] > 0) { // stat cannot be negative
+      stats[theStat]--;
+    }
+    notifyListeners();
+  }
+  
 }
 
 class LoginPage extends StatelessWidget {
@@ -68,16 +82,16 @@ class LoginPage extends StatelessWidget {
   }
 }
 
-class UpdateStatsPage extends StatelessWidget { // this page is where the team will input their stats while doing evangelism
+class UpdateStatsPage extends StatelessWidget { // this page is where the team will input their stats while doing outreach
   @override
   Widget build(BuildContext context) {
-    // var appState = context.watch<MyAppState>();
+    var appState = context.watch<MyAppState>();
 
-    return Scaffold(
+    return Scaffold( // TODO: update this to be a ListTile (so much easier...)
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Row( // TODO: this row should contain the headers 'solo' and 'partner' to identify the counters
+          Row( // row for headers
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text('Solo'),
@@ -88,28 +102,32 @@ class UpdateStatsPage extends StatelessWidget { // this page is where the team w
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text('Initiations:'),
-              Text('0'),
-              Text('0'),
+              Text(appState.stats[0].toString()), // solo text
+              Text(appState.stats[1].toString()), // partner text
               OutlinedButton( // button to increase solo init stat
                 onPressed: () {
+                  appState.incStat(0); // increment init solo
                   print('solo init + pressed');
                 },
                 child: Text('+ Solo'),
               ),
               OutlinedButton( // button to decrease solo init stat
                 onPressed: () {
+                  appState.decStat(0); // decrement init solo
                   print('solo init - pressed');
                 },
                 child: Text('- Solo'),
               ),
               OutlinedButton( // button to increase partner init stat
                 onPressed: () {
+                  appState.incStat(1);
                   print('partner init + pressed');
                 },
                 child: Text('+ Partner'),
               ),
               OutlinedButton( // button to decrease partner init stat
                 onPressed: () {
+                  appState.decStat(1);
                   print('partner init - pressed');
                 },
                 child: Text('- Partner'),
@@ -120,28 +138,32 @@ class UpdateStatsPage extends StatelessWidget { // this page is where the team w
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text('Spiritual Conversations:'),
-              Text('0'),
-              Text('0'),
+              Text(appState.stats[2].toString()),
+              Text(appState.stats[3].toString()),
               OutlinedButton( // button to increase solo spir convo stat
                 onPressed: () {
+                  appState.incStat(2);
                   print('solo spir convo + pressed');
                 },
                 child: Text('+ Solo'),
               ),
               OutlinedButton( // button to decrease solo spir convo stat
                 onPressed: () {
+                  appState.decStat(2);
                   print('solo spir convo - pressed');
                 },
                 child: Text('- Solo'),
               ),
               OutlinedButton( // button to increase partner spir convo stat
                 onPressed: () {
+                  appState.incStat(3);
                   print('partner spir convo + pressed');
                 },
                 child: Text('+ Partner'),
               ),
               OutlinedButton( // button to decrease partner spir convo stat
                 onPressed: () {
+                  appState.decStat(3);
                   print('partner spir convo - pressed');
                 },
                 child: Text('- Partner'),
@@ -152,28 +174,32 @@ class UpdateStatsPage extends StatelessWidget { // this page is where the team w
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text('Gospel Conversations:'),
-              Text('0'),
-              Text('0'),
+              Text(appState.stats[4].toString()),
+              Text(appState.stats[5].toString()),
               OutlinedButton( // button to increase solo GC stat
                 onPressed: () {
+                  appState.incStat(4);
                   print('solo GC + pressed');
                 },
                 child: Text('+ Solo'),
               ),
               OutlinedButton( // button to decrease solo GC stat
                 onPressed: () {
+                  appState.decStat(4);
                   print('solo GC - pressed');
                 },
                 child: Text('- Solo'),
               ),
               OutlinedButton( // button to increase partner GC stat
                 onPressed: () {
+                  appState.incStat(5);
                   print('partner GC + pressed');
                 },
                 child: Text('+ Partner'),
               ),
               OutlinedButton( // button to decrease partner GC stat
                 onPressed: () {
+                  appState.decStat(5);
                   print('partner GC - pressed');
                 },
                 child: Text('- Partner'),
@@ -184,28 +210,32 @@ class UpdateStatsPage extends StatelessWidget { // this page is where the team w
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               Text('Prayers to Receive Christ:'),
-              Text('0'),
-              Text('0'),
+              Text(appState.stats[6].toString()),
+              Text(appState.stats[7].toString()),
               OutlinedButton( // button to increase solo PRC stat
                 onPressed: () {
+                  appState.incStat(6);
                   print('solo PRC + pressed');
                 },
                 child: Text('+ Solo'),
               ),
               OutlinedButton( // button to decrease solo PRC stat
                 onPressed: () {
+                  appState.decStat(6);
                   print('solo PRC - pressed');
                 },
                 child: Text('- Solo'),
               ),
               OutlinedButton( // button to increase partner PRC stat
                 onPressed: () {
+                  appState.incStat(7);
                   print('partner PRC + pressed');
                 },
                 child: Text('+ Partner'),
               ),
               OutlinedButton( // button to decrease partner PRC stat
                 onPressed: () {
+                  appState.decStat(7);
                   print('partner PRC - pressed');
                 },
                 child: Text('- Partner'),
